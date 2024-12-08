d6f5b9fc2517ced117ba7fb6074ada3556965cbb
windows@home <programlfyh@proton.me>
2024-12-08 12:37:29 +0800

    build: rm image

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -2,9 +2,9 @@
 source .env
 
 image=$(docker inspect --format='{{.Config.Image}}' demeter)
-image_version="${image##*:}"
+CURRENT_DEMETER_VERSION="${image##*:}"
 
-if [ $image_version = $DEMETER_VERSION ]; then
+if [ $CURRENT_DEMETER_VERSION = $DEMETER_VERSION ]; then
     echo "please change version in .env"
 else
     sudo docker stop demeter
@@ -13,11 +13,14 @@ else
     sudo docker rm demeter
     echo "rm container demeter"
 
-    sudo docker build -t athena/demeter:$DEMETER_VERSION .
-    echo "build a new image: athena/demeter:$DEMETER_VERSION"
+    sudo docker image rm athena/demeter:$CURRENT_DEMETER_VERSION
+    echo "rm image athena/demeter:$CURRENT_DEMETER_VERSION"
 
     sudo docker volume rm demeter_results
     echo "rm a volume: demeter_results"
+
+    sudo docker build -t athena/demeter:$DEMETER_VERSION .
+    echo "build a new image: athena/demeter:$DEMETER_VERSION"
 
     sudo docker volume create demeter_results
     echo "create a volume: demeter_results"
