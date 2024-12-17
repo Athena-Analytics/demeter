14eb9b5f6ceef1977d8ad95f22c9274c4e185550
windows@company <programlfyh@proton.me>
2024-12-17 12:17:52 +0800

    build: change volume for config.ini

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -16,15 +16,15 @@ else
     sudo docker image rm athena/demeter:$CURRENT_DEMETER_VERSION
     echo "rm image athena/demeter:$CURRENT_DEMETER_VERSION"
 
-    sudo docker volume rm demeter_results
-    echo "rm a volume: demeter_results"
+    sudo docker volume rm demeter_app
+    echo "rm a volume: demeter_app"
 
     sudo docker build -t athena/demeter:$DEMETER_VERSION .
     echo "build a new image: athena/demeter:$DEMETER_VERSION"
 
-    sudo docker volume create demeter_results
-    echo "create a volume: demeter_results"
+    sudo docker volume create demeter_app
+    echo "create a volume: demeter_app"
 
-    sudo docker run -d -p 5000:5000 --name demeter --restart unless-stopped -v demeter_results:/app/src/demeter/results athena/demeter:$DEMETER_VERSION
+    sudo docker run -d -p 5000:5000 --name demeter --restart unless-stopped -v demeter_app:/app/src/demeter athena/demeter:$DEMETER_VERSION
     echo "run a new container: demeter"
 fi
